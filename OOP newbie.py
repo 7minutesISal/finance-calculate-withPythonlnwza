447c6da9a500c84049fabaf976ec1da350299125
Mylore <zazamoo256@gmail.com>
2026-04-03 00:01:57 +0700

    delete input data

--- OOP newbie.py
+++ OOP newbie.py
@@ -1,11 +1,4 @@
-datainput = ["FV","I","N"]
 
-def getinput(datainput):
-    value = []
-    for i in range(len(datainput)):
-        dataReal = int(input(f"{i}"))
-        value.append(dataReal)
-    return value
 
 class calculateFun :
     def __init__(self,FV,I,N):
@@ -16,8 +9,6 @@ class calculateFun :
     def calculatePV(self):
         pv = self.fv * (1 / ((1 + self.i) ** self.n))
         return pv
-
-FV,I,N = getinput("FV:","I:","N:")
 
 PVdata = calculatefun(FV,I,N)
 data = PVdata.calculatePV()
